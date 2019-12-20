9980611ee1b949f6cacd093859a8d1567a07873c
Alan Gerónimo <contacto@alangeronimo.com>
2019-12-19 23:07:56 -0500

    terminado las interfaces, solo falta tratar de enfriar o ajustar la temperatura con un timer

--- app/src/main/java/caja/alanger/cooler/views/LoadingActivity.kt
+++ app/src/main/java/caja/alanger/cooler/views/LoadingActivity.kt
@@ -1,24 +1,27 @@
 package caja.alanger.cooler.views
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import caja.alanger.cooler.R
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_loading.*
-import kotlinx.android.synthetic.main.activity_main.fabMain
 
 class LoadingActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loading2)
+        setContentView(R.layout.activity_loading)
 
+        /*
         fabMain.setOnClickListener {
             startActivity(Intent(this@LoadingActivity, ListAppsActivity::class.java))
             finish()
         }
+         */
 
         MobileAds.initialize(this)
 
@@ -28,5 +31,38 @@ class LoadingActivity : Activity() {
         val adRequestBottom = AdRequest.Builder().build()
         adViewBottom.loadAd(adRequestBottom)
 
+        startProgress()
+
+    }
+
+    val handler = Handler()
+    var progreso : Int = 0
+
+    val runa = object : Runnable {
+        @SuppressLint("SetTextI18n")
+        override fun run() {
+
+            if(progreso==100){
+                startActivity(Intent(this@LoadingActivity, ListAppsActivity::class.java))
+                finish()
+                handler.removeCallbacks(this)
+            }else{
+                progreso++
+                tViewProgress.text = "$progreso%"
+                handler.postDelayed(this, 50)
+            }
+
+
+        }
+    }
+
+    fun startProgress() {
+        handler.removeCallbacks(runa)
+        handler.postDelayed(runa, 200)
+    }
+
+    override fun onBackPressed() {
+        handler.removeCallbacks(runa)
+        super.onBackPressed()
     }
 }
--- app/src/main/java/caja/alanger/cooler/views/MainActivity.kt
+++ app/src/main/java/caja/alanger/cooler/views/MainActivity.kt
@@ -10,24 +10,23 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.os.*
 import android.os.Build
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import caja.alanger.cooler.R
 import caja.alanger.cooler.services.ServiceReader
 import caja.alanger.cooler.utils.*
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
 import com.google.android.gms.ads.MobileAds
 import jp.takke.cpustats.C
-import jp.takke.cpustats.C.intervalUpdate
-import kotlinx.android.synthetic.main.activity_list_apps.*
 import kotlinx.android.synthetic.main.activity_main.adView
+import kotlinx.android.synthetic.main.activity_main.fabMain
 import java.text.DecimalFormat
 
 
 class MainActivity : Activity() {
 
-
+    private lateinit var mInterstitialAd: InterstitialAd
 
     private var processesMode: Int = 0
 
@@ -49,7 +48,26 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
         startService(Intent(this, ServiceReader::class.java))
 
+
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = getString(R.string.interstitial_ad_unit_id)
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+
         mHandler = Handler()
+        val runShowInterestialForce = object : Runnable {
+            @SuppressLint("SetTextI18n")
+            override fun run() {
+                if (mInterstitialAd.isLoaded) {
+                    mInterstitialAd.show()
+                    mHandler.removeCallbacks(this)
+                }else{
+                    mHandler.postDelayed(this, 50)
+                }
+            }
+        }
+        mHandler.post(
+            runShowInterestialForce
+        )
 
         fabMain.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
@@ -65,8 +83,11 @@ class MainActivity : Activity() {
     }
     private fun setTextLabelMemory( percent: TextView, values: List<String>) {
         if (!values.isEmpty()) {
+            var ram = (Integer.parseInt(values[0]) * 100.0f / mSR.memTotal).toDouble()
             percent.text =
-                (  mFormatPercent.format((Integer.parseInt(values[0]) * 100.0f / mSR.memTotal).toDouble()) )+ C.percent
+                (  mFormatPercent.format( ram)+ C.percent)
+            main_tViewTemp.text = ""+mFormatPercent.format(ram*1.2)+"°"
+
         }else{
             Toast.makeText(this,"vacio",Toast.LENGTH_SHORT).show()
         }
@@ -79,35 +100,6 @@ class MainActivity : Activity() {
 
     }
 
-/*
-    private val drawRunnable = object : Runnable {
-        @SuppressLint("NewApi")
-        override fun run() {
-            mHandler.postDelayed(this, intervalUpdate.toLong())
-            if (mSR != null) { // finish() could have been called from the BroadcastReceiver
-                //mHandlerVG.post(drawRunnableGraphic)
-
-                setTextLabelCPU(null, mTVCPUTotalP, mSR.getCPUTotalP())
-                if (processesMode == C.processesModeShowCPU)
-                    setTextLabelCPU(null, mTVCPUAMP, mSR.getCPUAMP())
-                else
-                    setTextLabelCPU(null, mTVCPUAMP, null, mSR.getMemoryAM())
-
-                setTextLabelMemory(mTVMemUsed, mTVMemUsedP, mSR.getMemUsed())
-                setTextLabelMemory(mTVMemAvailable, mTVMemAvailableP, mSR.getMemAvailable())
-                setTextLabelMemory(mTVMemFree, mTVMemFreeP, mSR.getMemFree())
-                setTextLabelMemory(mTVCached, mTVCachedP, mSR.getCached())
-                setTextLabelMemory(mTVThreshold, mTVThresholdP, mSR.getThreshold())
-
-                for (n in 0 until mLProcessContainer.getChildCount()) {
-                    val l = mLProcessContainer.getChildAt(n) as LinearLayout
-                    setTextLabelCPUProcess(l)
-                    setTextLabelMemoryProcesses(l)
-                }
-            }
-        }
-    }
-*/
     private val mServiceConnection = object : ServiceConnection {
 
         @SuppressLint("NewApi")
@@ -115,22 +107,6 @@ class MainActivity : Activity() {
 
             mSR = (service as ServiceReader.ServiceReaderDataBinder).getService()
 
-/*
-            mVG.setService(mSR)
-            mVG.setParameters(cpuTotal, cpuAM, memUsed, memAvailable, memFree, cached, threshold)
-*/
- //           setIconRecording()
-         //   main_tViewRAMP.setText(mFormat.format(mSR.getMemTotal()) + C.kB)
-/*
-            switchParameter(cpuTotal, mLCPUTotal)
-            switchParameter(cpuAM, mLCPUAM)
-
-            switchParameter(memUsed, mLMemUsed)
-            switchParameter(memAvailable, mLMemAvailable)
-            switchParameter(memFree, mLMemFree)
-            switchParameter(cached, mLCached)
-            switchParameter(threshold, mLThreshold)
-*/
             mRunnable = Runnable {
 
 
@@ -165,37 +141,6 @@ class MainActivity : Activity() {
             )
 
 
-            // When on ActivityProcesses the screen is rotated, ActivityMain is destroyed and back is pressed from ActivityProcesses
-            // mSR isn't ready before onActivityResult() is called. So the Intent is saved till mSR is ready.
-
-/*
-            if (tempIntent != null) {
-                tempIntent.putExtra(C.screenRotated, true)
-                onActivityResult(1, 1, tempIntent)
-                tempIntent = null
-            } else
-                onActivityResult(1, 1, null)
-
-            if (Build.VERSION.SDK_INT >= 16) {
-                mLProcessContainer.getViewTreeObserver()
-                    .addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-                        override fun onGlobalLayout() {
-                            mLProcessContainer.getViewTreeObserver()
-                                .removeGlobalOnLayoutListener(this)
-                            val lt = LayoutTransition()
-                            lt.enableTransitionType(LayoutTransition.APPEARING)
-                            lt.enableTransitionType(LayoutTransition.DISAPPEARING)
-                            lt.enableTransitionType(LayoutTransition.CHANGING)
-                            mLProcessContainer.setLayoutTransition(lt)
-                            val lt2 = LayoutTransition()
-                            lt2.enableTransitionType(LayoutTransition.CHANGING)
-                            lt2.setStartDelay(LayoutTransition.CHANGING, 300)
-                            (mLProcessContainer.getParent() as LinearLayout).layoutTransition = lt2
-                        }
-                    })
-            }
-
-             */
         }
 
         override fun onServiceDisconnected(className: ComponentName) {
